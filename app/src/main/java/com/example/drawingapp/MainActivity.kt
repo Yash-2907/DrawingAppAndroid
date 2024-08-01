944694a42227353d1745ca58341f13da23b06244
Yash-2907 <guptayp_1@rknec.edu>
2024-08-01 21:41:50 +0530

    brush selector

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.drawingapp
 
+import android.app.Dialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var DrawObj:DrawingView
@@ -11,5 +14,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         DrawObj=findViewById<DrawingView>(R.id.Canvasid)
         //DrawObj.setBrushSize(25F)
+        findViewById<ImageButton>(R.id.brushbtn).setOnClickListener {
+            val brushselector:Dialog=Dialog(this)
+            brushselector.setContentView(R.layout.dialog_brushsizebox)
+            brushselector.setTitle("Brush Size")
+            brushselector.findViewById<ImageButton>(R.id.smallbtn).setOnClickListener{
+                DrawObj.setBrushSize(10f)
+                brushselector.dismiss()
+            }
+            brushselector.findViewById<ImageButton>(R.id.mediumbtn).setOnClickListener{
+                DrawObj.setBrushSize(30f)
+                brushselector.dismiss()
+            }
+            brushselector.findViewById<ImageButton>(R.id.largebtn).setOnClickListener{
+                DrawObj.setBrushSize(50f)
+                brushselector.dismiss()
+            }
+            brushselector.show()
+        }
     }
 }
