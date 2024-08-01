64957647c01e836c0682745b847401592997e836
Yash-2907 <guptayp_1@rknec.edu>
2024-08-01 22:31:11 +0530

    brush selector slider

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     lateinit var DrawObj:DrawingView
@@ -13,22 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DrawObj=findViewById<DrawingView>(R.id.Canvasid)
+        var lastval:Float=0f
         //DrawObj.setBrushSize(25F)
         findViewById<ImageButton>(R.id.brushbtn).setOnClickListener {
             val brushselector:Dialog=Dialog(this)
             brushselector.setContentView(R.layout.dialog_brushsizebox)
             brushselector.setTitle("Brush Size")
-            brushselector.findViewById<ImageButton>(R.id.smallbtn).setOnClickListener{
-                DrawObj.setBrushSize(10f)
-                brushselector.dismiss()
-            }
-            brushselector.findViewById<ImageButton>(R.id.mediumbtn).setOnClickListener{
-                DrawObj.setBrushSize(30f)
-                brushselector.dismiss()
-            }
-            brushselector.findViewById<ImageButton>(R.id.largebtn).setOnClickListener{
-                DrawObj.setBrushSize(50f)
-                brushselector.dismiss()
+            brushselector.findViewById<Slider>(R.id.sliderbtn).value=lastval
+            brushselector.findViewById<Slider>(R.id.sliderbtn).addOnChangeListener{Slider,value,fromUser->
+                DrawObj.setBrushSize(value)
+                lastval=value
             }
             brushselector.show()
         }
