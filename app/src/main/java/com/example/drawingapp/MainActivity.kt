08b279f9ce068ee2d4d903b6ce4adbe787a00f07
Yash-2907 <guptayp_1@rknec.edu>
2024-08-03 00:43:04 +0530

    resizer changed interface

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -8,16 +8,18 @@ import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import com.google.android.material.slider.Slider
 import yuku.ambilwarna.AmbilWarnaDialog
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var DrawObj:DrawingView
+    var brushOReraser :Boolean=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DrawObj=findViewById<DrawingView>(R.id.Canvasid)
-        var lastval:Float=0f
-        //DrawObj.setBrushSize(25F)
-        var defaultcolor=ContextCompat.getColor(this,R.color.white)
+        var lastvalbrush:Float=10f
+        var lastvaleraser:Float=10f
+        var defaultcolor=ContextCompat.getColor(this,R.color.black)
         findViewById<ImageButton>(R.id.colorpalletebtn).setOnClickListener{
             setselected(2)
             val colorpicker:AmbilWarnaDialog=AmbilWarnaDialog(this,defaultcolor,object : AmbilWarnaDialog.OnAmbilWarnaListener{
@@ -34,37 +36,28 @@ class MainActivity : AppCompatActivity() {
         }
         findViewById<ImageButton>(R.id.brushbtn).setOnClickListener {
             setselected(1)
-            val brushselector:Dialog=Dialog(this)
-            brushselector.setContentView(R.layout.dialog_brushsizebox)
-            brushselector.setTitle("Brush Size")
-            brushselector.findViewById<Slider>(R.id.sliderbtn).value=lastval
-            brushselector.findViewById<Slider>(R.id.sliderbtn).addOnChangeListener{Slider,value,fromUser->
-                DrawObj.setBrushSize(value)
-                lastval=value
-            }
-            brushselector.show()
-            brushselector.findViewById<ImageButton>(R.id.smallbtn).setOnClickListener{
-                DrawObj.setBrushSize(20F)
-                lastval=20f
-                brushselector.dismiss()
-            }
-            brushselector.findViewById<ImageButton>(R.id.mediumbtn).setOnClickListener{
-                DrawObj.setBrushSize(45F)
-                lastval=45f
-                brushselector.dismiss()
-            }
-            brushselector.findViewById<ImageButton>(R.id.largebtn).setOnClickListener{
-                DrawObj.setBrushSize(70F)
-                lastval=70f
-                brushselector.dismiss()
-            }
+            brushOReraser=true
+            findViewById<Slider>(R.id.sizeslider).value=lastvalbrush
+            DrawObj.setBrushColor(defaultcolor)
         }
         findViewById<ImageButton>(R.id.eraserbtn).setOnClickListener{
             setselected(3)
+            brushOReraser=false
+            findViewById<Slider>(R.id.sizeslider).value=lastvaleraser
             val whitecolor=ContextCompat.getColor(this,R.color.white)
             DrawObj.setBrushColor(whitecolor)
         }
+        findViewById<Slider>(R.id.sizeslider).addOnChangeListener{slider,value,fromUser->
+            DrawObj.setBrushSize(value)
+            if(brushOReraser) {
+                lastvalbrush = value
+            }
+            else {
+                lastvaleraser = value
+            }
+        }
     }
+
     fun resetbtnselection()
     {
         findViewById<ImageButton>(R.id.brushbtn).background=ContextCompat.getDrawable(this,R.drawable.roundbtn)
@@ -75,7 +68,8 @@ class MainActivity : AppCompatActivity() {
     {
         resetbtnselection()
         when(n){
-            1->{findViewById<ImageButton>(R.id.brushbtn).background=ContextCompat.getDrawable(this,R.drawable.roundbtnselected)}
+            1->{findViewById<ImageButton>(R.id.brushbtn).background=ContextCompat.getDrawable(this,R.drawable.roundbtnselected)
+            }
             2->{findViewById<ImageButton>(R.id.colorpalletebtn).background=ContextCompat.getDrawable(this,R.drawable.roundbtnselected)}
             3->{findViewById<ImageButton>(R.id.eraserbtn).background=ContextCompat.getDrawable(this,R.drawable.roundbtnselected)}
         }
